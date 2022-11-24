8cca91f330810881e5d67c8e5ae290db8005df22
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-11-24 08:09:34 +0100

    Add changes

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,11 +1,20 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import {useCallback, useState} from 'react';
 
-const WeatherBox = props => {
+
+const WeatherBox = (props) => {
+
+  const handleCityChange = useCallback;
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}e35d409a8d6af9bd0a395b47b6f0ec3b&units=metric`)
+   .then(res => res.json())
+   .then(data => {
+     console.log(data);
+   });
   return (
     <section>
-      <PickCity />
+      <PickCity action={handleCityChange} />
       <WeatherSummary />
       <Loader />
     </section>
